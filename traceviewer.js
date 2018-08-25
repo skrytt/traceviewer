5ac503515cf84d28e5b1f8abed8cade80f1b6e42
skrytt <matt.kemp.1992@gmail.com>
2018-08-25 14:09:16 +0100

    Use Vue to create row components

--- traceviewer.js
+++ traceviewer.js
@@ -1,8 +1,23 @@
+Vue.component('viewer-row', {
+  props: ['label', 'start', 'duration'],
+  template: `
+    <div class="viewer-data-row">
+      <span class="viewer-row-entry viewer-row-label">{{label}}</span>
+      <span class="viewer-row-entry viewer-data-row-start-delay" v-bind:style="{width: start}"></span>
+      <span class="viewer-row-entry viewer-data-row-duration" v-bind:style="{width: duration}"></span>
+    </div>
+  `
+})
+
 window.addEventListener('load', function() {
   var app = new Vue({
-      el: '#app',
-      data: {
-          message: 'Welcome! The time is ' + new Date().toLocaleString()
-      }
+    el: '#viewer-row-container',
+    data: {
+      rows: [
+        { id: 1, label: 'Foo', start: "0px", duration: "50px" },
+        { id: 2, label: 'Bar', start: "20px", duration: "40px" },
+        { id: 3, label: 'Baz', start: "40px", duration: "60px" }
+      ]
+    }
   });
 })
